0a94856491ce04053eb45eda052c93f755d783af
ivan2kh <ivan2kh@gmail.com>
2018-04-26 20:02:50 +0300

    Use google maps. Black theme. Calc point weight.

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -1,12 +1,23 @@
 import csv
 import json
+from collections import Counter
 
 
 def csv2json():
+    points = Counter()
+    with open('data/points.csv') as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=',', fieldnames=['lat', 'lon', 'ip', 'host', 'date', 'url', 'w'])
+        # for row in spamreader:
+        #     print(row)
+        for row in csvreader:
+            coord = (row['lat'], row['lon'])
+            points[coord] += int(row['w'])
+
     with open('static/data/ips.json', 'w') as outfile:
-        with open('data/points.csv') as csvfile:
-            csvreader = csv.DictReader(csvfile, delimiter=',', fieldnames=['lat', 'lon', 'ip', 'host', 'date', 'url', 'w'])
-            # for row in spamreader:
-            #     print(row)
-            all_rows = [{'lat': row['lat'], 'lon': row['lon'], 'w': row['w']} for row in csvreader]
-            json.dump(all_rows, outfile)
+        outfile.write("data = ")
+        all_rows = [{'lat': coord[0], 'lon': coord[1], 'w': w} for coord, w in points.items()]
+        json.dump(all_rows, outfile)
+
+
+if __name__ == '__main__':
+    csv2json()
